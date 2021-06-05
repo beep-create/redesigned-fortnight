40dea6b43229eae289c16ab8e4bfb0d9bcb5027b
beep-create <77546458+beep-create@users.noreply.github.com>
2021-06-04 20:30:16 -0400

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -28,12 +28,22 @@ function logOut() {
 window.location="index.html"
 }
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {
-document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot)
- {childKey  = childSnapshot.key;
-     Room_names = childKey;
-    //Start code
+function redirectToRoomName(name) {
+  console.log(name);
+  localStorage.setItem("room_name", room_name);
+  window.location="kwitter_page.html";
+}
 
-    //End code
-    });});}
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {
+  document.getElementById("output").innerHTML = "";
+  snapshot.forEach(function(childSnapshot)
+   {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+      console.log("Room Name - " + Room_names); 
+      row = "<div class='room_name' id="+ Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>"; 
+      document.getElementById("output").innerHTML += row; 
+      //End code
+      });});}
 getData();
+var ok = "was ist dein lieblingsfach";
